3abee64a921c00fa615aba2eb2e4baae256cb97f
Brandon <bcollaguazos@est.ups.edu.ec>
2025-06-17 08:50:08 -0500

    main

---
+++ src/Materia/controllers/ArbolBinario.java
@@ -0,0 +1,46 @@
+package Materia.controllers;
+
+import Materia.models.Node;
+
+public class ArbolBinario {
+
+    private Node root;
+
+    public ArbolBinario() {
+        this.root = null;
+    }
+
+    public void insert(int value) {
+        root = insertRec(root, value);
+    }
+
+    private Node insertRec(Node padre, int value) {
+        if(padre == null) {
+            return new Node(value);
+        }
+
+        if(value < padre.getValue()) {
+            //Me voy a la izquierda
+            padre.setLeft(insertRec(padre.getLeft(), value));
+
+        } else if(value > padre.getValue()) {
+            //Me voy a la derecha
+            padre.setRight(insertRec(padre.getRight(), value));
+        }
+
+        return padre;
+    }
+
+    public void imprimirArbol() {
+        imprimir(root);
+        System.out.println();
+    }
+
+    private void imprimir(Node node) {
+        if(node != null) {
+            imprimir(node.getLeft());
+            System.out.print(node.getValue() + " ");
+            imprimir(node.getRight());
+        }
+    }
+}
